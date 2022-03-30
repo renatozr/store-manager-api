52e1ee68848083622a259789fc64d46a53e080a8
renatozr <renatozr07@gmail.com>
2022-03-30 15:13:31 -0300

    iniciando projeto Store Manager

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Escutando na porta ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
